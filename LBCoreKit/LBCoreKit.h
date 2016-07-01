3a49c166494602d3080d6cb96bb6a79c441c1879
jianglan <jianglan_job@163.com>
2016-07-01 14:33:28 +0800

    Initial Commit

---
+++ LBCoreKit/LBCoreKit.h
@@ -0,0 +1,19 @@
+//
+//  LBCoreKit.h
+//  LBCoreKit
+//
+//  Created by ZED-3 on 16/7/1.
+//  Copyright © 2016年 LonelyBanana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LBCoreKit.
+FOUNDATION_EXPORT double LBCoreKitVersionNumber;
+
+//! Project version string for LBCoreKit.
+FOUNDATION_EXPORT const unsigned char LBCoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LBCoreKit/PublicHeader.h>
+
+
